1932d377a281d0cd04f5989cc66bd3341ecdc35a
sa sa <betaw067@gmail.com>
2022-09-30 23:06:18 +1000

    FirstCommit

---
+++ WpfApp1/WpfApp1/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class User
+    {
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal DateTime Birthdate { get; set; }
+        internal string Address { get; set; }
+        internal User(int id, string name, DateTime birthdate, string address)
+        {
+            Id = id;
+            Name = name;
+            Birthdate = birthdate;
+            Address = address;
+        }
+    }
+}
